ef23367a24e2162189a19912b78ecf29c029a182
Dave <daveclydesdale@gmail.com>
2019-05-29 22:51:48 +0200

    https

--- ui.js
+++ ui.js
@@ -30,28 +30,28 @@ class UI {
     const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday','Monday','Tuesday'];
     const months = ['January','Febuary','March','April','May','June','July','August','September','October','November','December']
 
-    let timeFormated = `${days[time.getDay()]} ${time.getDate()}th ${months[time.getMonth()]} ${time.getHours()}:${time.getMinutes()}`// Saturday, September 17, 2016
-
-    const f1 = days[time.getDay()+1]
-    const f2 = days[time.getDay()+2]
-    const f3 = days[time.getDay()+3]
+    let timeFormated = `${days[time.getDay()]} ${time.getDate()}th ${months[time.getMonth()]} ${time.getHours()}:${time.getMinutes()}`
     
-
+    ///main header
     this.location.textContent = weather.location.name;
-    this.localTime.textContent = `Local time: ${timeFormated}`
     this.description.textContent = weather.current.condition.text;
     this.temp.textContent = `${weather.current.temp_c.toFixed(1)}º`
-    this.icon.src=weather.current.condition.icon
+    this.icon.src=`https:${weather.current.condition.icon}`;
+    this.localTime.textContent = `Local time: ${timeFormated}`
+
+    ///main numbers
+    this.humidity.textContent=`Feels like: ${weather.current.feelslike_c}º`
     this.maxTemp.textContent=`Max temp: ${weather.forecast.forecastday[0].day.maxtemp_c.toFixed(1)}º`;
     this.minTemp.textContent=`Min temp: ${weather.forecast.forecastday[0].day.mintemp_c.toFixed(1)}º`;
-    this.humidity.textContent=`Feels like: ${weather.current.feelslike_c}º`
     this.astro.textContent=`sunrise: ${weather.forecast.forecastday[0].astro.sunrise}, sunset: ${weather.forecast.forecastday[0].astro.sunset} `
-    this.forecast1.innerText=f1
-    this.forecast2.innerText=f2
-    this.forecast3.innerText=f3
-    this.fImg1.src=weather.forecast.forecastday[1].day.condition.icon;
-    this.fImg2.src=weather.forecast.forecastday[2].day.condition.icon;
-    this.fImg3.src=weather.forecast.forecastday[3].day.condition.icon;
+
+    /// 3-day forecast
+    this.forecast1.innerText=days[time.getDay()+1]
+    this.forecast2.innerText=days[time.getDay()+2]
+    this.forecast3.innerText=days[time.getDay()+3]
+    this.fImg1.src=`https:${weather.forecast.forecastday[1].day.condition.icon}`;
+    this.fImg2.src=`https:${weather.forecast.forecastday[2].day.condition.icon}`;
+    this.fImg3.src=`https:${weather.forecast.forecastday[3].day.condition.icon}`;
     this.foreInner1.innerText=weather.forecast.forecastday[1].day.condition.text;
     this.foreInner2.innerText=weather.forecast.forecastday[2].day.condition.text;
     this.foreInner3.innerText=weather.forecast.forecastday[3].day.condition.text;
